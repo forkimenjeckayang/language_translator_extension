9ff7cf3f0ebc5a93ae0fbbd7051e9f7f1bb12a15
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-06-10 16:57:25 +0100

    modified Starter files

--- background.js
+++ background.js
@@ -1,28 +1,27 @@
 // Create context menu item
 chrome.contextMenus.create({
-    id: "translateText",
-    title: "Translate with Language Translator Extension",
-    contexts: ["selection"] // Show context menu item when text is selected
-  });
-  
-  // Add click event listener for context menu item
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "translateText" && info.selectionText) {
-      // Send selected text to the popup for translation
-      chrome.runtime.sendMessage({ type: "translateFromContextMenu", text: info.selectionText });
-    }
-  });
+  id: "translateText",
+  title: "Translate with Language Translator Extension",
+  contexts: ["selection"] // Show context menu item when text is selected
+});
 
-  // Add listener for messages from content script
+// Add click event listener for context menu item
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "translateText" && info.selectionText) {
+    // Send selected text to the popup for translation
+    chrome.runtime.sendMessage({ type: "translateFromContextMenu", text: info.selectionText });
+  }
+});
+
+// Add listener for messages from content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "textSelected" && message.text) {
-      // Create context menu item dynamically for selected text
-      chrome.contextMenus.create({
-        id: "translateText",
-        title: `Translate "${message.text}" with Language Translator Extension`,
-        contexts: ["selection"]
-      });
-    }
-  });
-  
-  
+  if (message.type === "textSelected" && message.text) {
+    // Create context menu item dynamically for selected text
+    chrome.contextMenus.create({
+      id: "translateText",
+      title: `Translate "${message.text}" with Language Translator Extension`,
+      contexts: ["selection"]
+    });
+  }
+});
+
